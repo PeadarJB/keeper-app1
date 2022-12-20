4d456cfc0429ebb61d53cb187d9152b99006e92e
Peadar Jolliffe-Byrne <peadarjb@gmail.com>
2022-12-19 21:12:03 -0600

    Slight edit
    Changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
     return (
         <header>
-            <h1>Hello World</h1>
+            <h1>Keeper</h1>
         </header>
     );
     }
